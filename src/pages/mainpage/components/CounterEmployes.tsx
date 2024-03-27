d1b5212733e3cd008f14b32fb8caccf1944fe644
Tempo-Tempo <4mo.4mo@list.ru>
2024-03-27 16:17:34 +0500

    test

--- src/pages/mainpage/components/CounterEmployes.tsx
+++ src/pages/mainpage/components/CounterEmployes.tsx
@@ -1,10 +1,102 @@
 import React from 'react';
+import { Images } from '../../../shared/ui/images/Images.tsx';
+import { Texts, TextAlign, TextSize } from '../../../shared/ui/texts/Texts.tsx';
+import helpersImg from '../../../shared/icons/helpers.svg';
+import sysAdmImg from '../../../shared/icons/sys.admin.svg';
+import ingenerImg from '../../../shared/icons/ingeners.svg';
+import buisnesImg from '../../../shared/icons/buis.analit.svg';
+import developImg from '../../../shared/icons/dev.svg';
+import './CounterEmployes.css';
 
 export const CounterEmployes = () => {
    return (
-      <div>
-         
+      <div className='counters_employes'>
+      <div className='counter'>
+         <div className='title_counter'>
+         <span>
+            <Images width={70} height={60} img={helpersImg}/>
+         </span>
+         <span className='text_counter'>
+         <Texts width={500} size={TextSize.XL} color='black' text='125'/>
+         </span>
       </div>
+      <div className='body_counter'>
+      <Texts 
+            text='Специалистов поддержки пользователей'
+            color={'gray'}
+            align={TextAlign.center}
+            />
+      </div>
+     </div>
+     <div className='counter'>
+         <div className='title_counter'>
+         <span>
+         <Images width={70} height={60} img={sysAdmImg}/>
+         </span>
+         <span className='text_counter'>
+            <Texts width={500} size={TextSize.XL} color='black' text='125'/>
+         </span>
+      </div>
+      <div className='body_counter'>
+      <Texts 
+            text='Системных администраторов'
+            color={'gray'}
+            align={TextAlign.center}
+            />
+      </div>
+     </div> 
+     <div className='counter'>
+         <div className='title_counter'>
+         <span>
+         <Images width={70} height={60} img={ingenerImg}/>
+         </span>
+         <span className='text_counter'>
+         <Texts width={500} size={TextSize.XL} color='black' text='125'/>
+         </span>
+      </div>
+      <div className='body_counter'>
+      <Texts 
+            text='Инженеров спопровождения и развития'
+            color={'gray'}
+            align={TextAlign.center}
+            />
+      </div>
+     </div>
+     <div className='counter'>
+         <div className='title_counter'>
+         <span>
+         <Images width={70} height={60} img={buisnesImg}/>
+         </span>
+         <span className='text_counter'>
+         <Texts width={500} size={TextSize.XL} color='black' text='125'/>
+         </span>
+      </div>
+      <div className='body_counter'>
+      <Texts 
+            text='Бизнес/Системных-аналитиков'
+            color={'gray'}
+            align={TextAlign.center}
+            />
+      </div>
+     </div> 
+     <div className='counter'>
+         <div className='title_counter'>
+         <span>
+         <Images width={70} height={60} img={developImg}/>
+         </span>
+         <span className='text_counter'>
+         <Texts width={500} size={TextSize.XL} color='black' text='125'/>
+         </span>
+      </div>
+      <div className='body_counter'>
+      <Texts 
+            text='Разработчиков'
+            color={'gray'}
+            align={TextAlign.center}
+            />
+      </div>
+     </div> 
+   </div>
    );
 };
 
--- src/pages/mainpage/components/HistoryCompany.tsx
+++ src/pages/mainpage/components/HistoryCompany.tsx
@@ -3,11 +3,7 @@ import './HistoryCompany.css';
 import { Texts, TextAlign, TextSize } from '../../../shared/ui/texts/Texts.tsx';
 import { Images } from '../../../shared/ui/images/Images.tsx';
 import img from '../../../shared/assets/history.png';
-import helpersImg from '../../../shared/icons/helpers.svg';
-import sysAdmImg from '../../../shared/icons/sys.admin.svg';
-import ingenerImg from '../../../shared/icons/ingeners.svg';
-import buisnesImg from '../../../shared/icons/buis.analit.svg';
-import developImg from '../../../shared/icons/dev.svg';
+import { CounterEmployes } from './CounterEmployes.tsx';
 
 export const HistoryCompany = () => {
    return (
@@ -44,93 +40,7 @@ export const HistoryCompany = () => {
                   <Images width={550} img={img}/>
                </div>
             </div>
-            <div className='counters_employes'>
-               <div className='counter'>
-                  <div className='title_counter'>
-                  <span>
-                     <Images width={70} height={60} img={helpersImg}/>
-                  </span>
-                  <span className='text_counter'>
-                  <Texts width={500} size={TextSize.XL} color='black' text='125'/>
-                  </span>
-               </div>
-               <div className='body_counter'>
-               <Texts 
-                     text='Специалистов поддержки пользователей'
-                     color={'gray'}
-                     align={TextAlign.center}
-                     />
-               </div>
-              </div>
-              <div className='counter'>
-                  <div className='title_counter'>
-                  <span>
-                  <Images width={70} height={60} img={sysAdmImg}/>
-                  </span>
-                  <span className='text_counter'>
-                     <Texts width={500} size={TextSize.XL} color='black' text='125'/>
-                  </span>
-               </div>
-               <div className='body_counter'>
-               <Texts 
-                     text='Системных администраторов'
-                     color={'gray'}
-                     align={TextAlign.center}
-                     />
-               </div>
-              </div> 
-              <div className='counter'>
-                  <div className='title_counter'>
-                  <span>
-                  <Images width={70} height={60} img={ingenerImg}/>
-                  </span>
-                  <span className='text_counter'>
-                  <Texts width={500} size={TextSize.XL} color='black' text='125'/>
-                  </span>
-               </div>
-               <div className='body_counter'>
-               <Texts 
-                     text='Инженеров спопровождения и развития'
-                     color={'gray'}
-                     align={TextAlign.center}
-                     />
-               </div>
-              </div>
-              <div className='counter'>
-                  <div className='title_counter'>
-                  <span>
-                  <Images width={70} height={60} img={buisnesImg}/>
-                  </span>
-                  <span className='text_counter'>
-                  <Texts width={500} size={TextSize.XL} color='black' text='125'/>
-                  </span>
-               </div>
-               <div className='body_counter'>
-               <Texts 
-                     text='Бизнес/Системных-аналитиков'
-                     color={'gray'}
-                     align={TextAlign.center}
-                     />
-               </div>
-              </div> 
-              <div className='counter'>
-                  <div className='title_counter'>
-                  <span>
-                  <Images width={70} height={60} img={developImg}/>
-                  </span>
-                  <span className='text_counter'>
-                  <Texts width={500} size={TextSize.XL} color='black' text='125'/>
-                  </span>
-               </div>
-               <div className='body_counter'>
-               <Texts 
-                     text='Разработчиков'
-                     color={'gray'}
-                     align={TextAlign.center}
-                     />
-               </div>
-              </div> 
-            </div>
+            <CounterEmployes />
          </div>
       </div>
    );
