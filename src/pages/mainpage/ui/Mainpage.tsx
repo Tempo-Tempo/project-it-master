1d3a811ec48ee7f94cd3e20c68ca7b5c09fa4790
Tempo-Tempo <4mo.4mo@list.ru>
2024-04-05 13:27:22 +0500

    add scrollTo

--- src/pages/mainpage/ui/Mainpage.tsx
+++ src/pages/mainpage/ui/Mainpage.tsx
@@ -9,43 +9,64 @@ import { VideosAboutWork } from '../components/VideosAboutWork';
 import { SubscribeMe } from '../components/SubscribeMe';
 import { Footerbar } from 'widget/footerbar';
 import { FormForFeedback } from 'features/formForFeedback';
+import { useInView } from 'react-intersection-observer';
+import top from 'shared/icons/top-arrow.svg';
+import { Images } from 'shared/ui/images/Images';
 import './Mainpage.css';
 
 const Mainpage = () => {
 
-   const [isOpen, setIsOpen] = useState(false);
+   const [isOpen, setIsOpen] = useState<boolean>(false);
 
    const isClose = () => {
       setIsOpen(false);
       return;
    }
 
+    const {ref, inView} = useInView({
+      threshold: 0.2,
+   })
+
+   const btnStyle = inView ? 'activeImg' : 'nonactiveImg';
+
    const modalIsOpen = () => {
       setIsOpen(prev => !prev);
+   }
+
+   const scrollToSection = (refName: string) => {
+      const anyRef = document.getElementById(refName) as HTMLElement | null;
+      anyRef?.scrollIntoView({
+         behavior: "smooth"
+      })
    }
 
    return (
       <div className='main_page'>
          <div className='main_modal'>
             <FormForFeedback isOpen={isOpen} isClose={() => isClose()}/>
-         </div>  
-         <section  className='main_navbar'>
-            <Navbar />
+         </div>
+         <div onClick={() => scrollToSection('navbar')} 
+         className={`top_btn ${btnStyle}`}>
+            <Images img={top}/>
+         </div> 
+         <section id='navbar' className='main_navbar'>
+            <Navbar scrollToSection={(refName: string) => scrollToSection(refName)}/>
          </section>
          <section className='main_banner'>
            <Banner modalIsOpen={modalIsOpen}/> 
          </section> 
          <div className='test' />
-         <section className='main_services'>
+         <div ref={ref}>
+            <section id='about_company' className='main_services'>
             <Services />
          </section>
-         <section className='main_history'>
+         <section id='history_company'  className='main_history'>
             <HistoryCompany />
          </section>
-         <section className='main_about_services'>
+         <section id='services' className='main_about_services'>
             <AbousServices modalIsOpen={modalIsOpen}/>
          </section>
-         <section className='main_ourworks'>
+         <section id='our_works' className='main_ourworks'>
             <OurWorks modalIsOpen={modalIsOpen}/>
          </section>
          <section className='main_videos_about_work'>
@@ -54,9 +75,11 @@ const Mainpage = () => {
          <section className='main_subscribe'>
            <SubscribeMe />
          </section>
-         <section className='main_footer'>
+         <section id='contacts' className='main_footer'>
            <Footerbar />
          </section>
+         </div>
+         
       </div>
    );
 };
--- src/widget/navbar/ui/Navbar.tsx
+++ src/widget/navbar/ui/Navbar.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import './Navbar.css';
 import { TextSize, Texts } from '../../../shared/ui/texts/Texts';
 import { useInView } from 'react-intersection-observer';
+import './Navbar.css';
 
-const Navbar = () => {
+
+interface NavbarProps {
+   scrollToSection: (anyRef: string) => void
+}
+
+const Navbar = ({ scrollToSection } : NavbarProps) => {
    const {ref, inView} = useInView({
       threshold: 0,
       triggerOnce: true
    })
 
+   const scrollToDiv = (id: string) => {
+      console.log('testNav');
+      scrollToSection(id);
+   }
+
    const testStyle = inView ? 'haeader_wrapper_active' : '';
+
    return (
       <div className={`haeader_wrapper ${testStyle}`}>
         <div  ref={ref}  className='container'>
@@ -18,19 +29,19 @@ const Navbar = () => {
                <Texts text='- МАСТЕР' size={TextSize.L}/>
             </span>
          <ul className='main_header'>
-            <nav>
+            <nav className='about_company' onClick={(e) => scrollToDiv(e.currentTarget.className)}>
+               <Texts text='О компании' size={TextSize.M}/>
+            </nav>
+            <nav className='history_company' onClick={(e) => scrollToDiv(e.currentTarget.className)}>
                <Texts text='Вакансии' size={TextSize.M}/>
             </nav>
-            <nav>
+            <nav className='our_works'  onClick={(e) => scrollToDiv(e.currentTarget.className)}>
                <Texts text='Наши проекты' size={TextSize.M}/>
             </nav>
-            <nav>
+            <nav className='services'  onClick={(e) => scrollToDiv(e.currentTarget.className)}>
                <Texts text='Услуги' size={TextSize.M}/>
             </nav>
-            <nav>
-               <Texts text='Новости' size={TextSize.M}/>    
-            </nav>
-            <nav>
+            <nav className='contacts'  onClick={(e) => scrollToDiv(e.currentTarget.className)}>
                <Texts text='Контакты' size={TextSize.M}/>
             </nav>
          </ul>
